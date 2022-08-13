a27a63b1f5a4c7abe414b9b1b87ddfc2c8a584f5
HeoYou <lov21y_@naver.com>
2022-08-13 21:19:25 +0900

    second commit

--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String test(){
-        return "Hello world";
+        return "Hello world Thank";
     }
 
 }
