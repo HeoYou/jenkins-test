cf079a820f806fae532dd9b62ba7f50243d511a0
HeoYou <lov21y_@naver.com>
2022-08-15 13:11:08 +0900

    다시

--- src/test/java/com/sellinone/sellinone/SellinoneApplicationTests.java
+++ src/test/java/com/sellinone/sellinone/SellinoneApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.sellinone.sellinone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SellinoneApplicationTests {
 
 	@Test
 	void contextLoads() {
